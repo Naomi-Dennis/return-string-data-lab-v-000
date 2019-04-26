f0ff31710cf7acc9ec191159ef42c7d305cedcf2
Naomi Dennis <njdennis94@gmail.com>
2019-04-26 00:04:03 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,1 +1,12 @@
-alert("Test")
+$(document).ready = init 
+
+function init(){
+  $(".js-more-buttdon").on("click", viewMore)
+}
+
+function viewMore(){
+  let id = $(this).data("id")
+  $.get(`/products/${id}/body`, (data) => {
+    console.log(data)
+  })
+}
